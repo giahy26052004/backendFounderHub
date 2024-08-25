4bf91a4a78f414372537088d720bb2b75c376d59
=giahy26052004 <=giahy26052004@gmail.com>
2024-08-25 23:45:29 +0700

    pushcode2

--- middlewares/protectRoute.js
+++ middlewares/protectRoute.js
@@ -2,12 +2,12 @@ import User from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 const protectRoute = async (req, res, next) => {
   try {
-    // const token = req.cookies.jwt;
-    // if (!token) {
-    //   return res.status(401).json({ message: "Unauthorizeds" });
-    // }
-    // const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    // const user = await User.findById(decoded.userId).select("-password");
+    const token = req.cookies.jwt;
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorizeds" });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId).select("-password");
     req.user = user;
     next();
   } catch (err) {
--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -7,12 +7,12 @@ import {
   deleteTask,
   editTask,
 } from "../controllers/tasksController.js";
-// import protectRoute from "../middlewares/protectRoute.js";
+import protectRoute from "../middlewares/protectRoute.js";
 const router = express.Router();
-router.get("/completed", getTasksCompleted); ///http://localhost:5000/api/tasks/completed
-router.get("/:id", getTaskById); ///http://localhost:5000/api/tasks/:id
-router.put("/:id", editTask); ///http://localhost:5000/api/tasks/:id
-router.get("/", getTasks); ///http://localhost:5000/api/tasks
-router.delete("/:id", deleteTask); ///http://localhost:5000/api/tasks
-router.post("/", registerTask); //http://localhost:5000/api/tasks
+router.get("/completed", protectRoute, getTasksCompleted); ///http://localhost:5000/api/tasks/completed
+router.get("/:id", protectRoute, getTaskById); ///http://localhost:5000/api/tasks/:id
+router.put("/:id", protectRoute, editTask); ///http://localhost:5000/api/tasks/:id
+router.get("/", protectRoute, getTasks); ///http://localhost:5000/api/tasks
+router.delete("/:id", protectRoute, deleteTask); ///http://localhost:5000/api/tasks
+router.post("/", protectRoute, registerTask); //http://localhost:5000/api/tasks
 export default router;
